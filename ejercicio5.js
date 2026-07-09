5066c18007cd509796ffab6382c0d282d372309e
Sebsatian Pereira <sebasperemald04@gmail.com>
2026-07-09 17:43:09 -0500

    Ejercicio 5 implementando algunos cambios - Archivo: ejercicio5.js

--- ejercicio5.js
+++ ejercicio5.js
@@ -1,44 +1,46 @@
 const prompt = require("prompt-sync")();
 
+//Variables auxiliares para el menú
 let stop = 1;
 let opcion = 0;
 
-function Vehiculo(modelo, marca, placa, color, kilometraje, encendido = false, velocidad = 0){
+function Vehiculo(modelo, marca, placa, color, kilometraje, encendido = false, velocidad = false){
     this.modelo = modelo;
     this.marca = marca;
     this.placa = placa;
     this.color = color;
     this.kilometraje = kilometraje;
-    this.kilometraje = kilometraje;
+    this.encendido = encendido;
     this.velocidad = velocidad;
 
     this.avanzar = function(){
-        if(this.encenderApagar) return this.velocidad += 1;
+        if(this.encendido) return this.velocidad += 1;
         return "Primero tienes que encender el vehiculo"
     }
 
     this.parar = function(){
-        if(this.encenderApagar && this.velocidad > 0) return this.velocidad -= 1
+        if(this.encendido && this.velocidad > 0) return this.velocidad -= 1
         return "El carro esta apagado o no tiene velocidad"
     }
 
     this.encenderApagar = function(){
-        if(this.encendido == false) return this.encendido = true
-        return false
+        if(this.encendido) return this.encendido = false;
+        return this.encendido = true;
     }
 
     this.informacionVehiculo = function(){
-        console.log("\n--- Datos del vehiculo ---");
-        console.log("Marca: ", this.marca);
-        console.log("Modelo: ", this.modelo);
-        console.log("Placa: ", this.placa);
-        console.log("Color: ", this.color);
-        console.log("kilometraje: ", this.kilometraje, "\n");
-        
+        return `
+            \n--- Datos del vehiculo ---\n
+    Marca:  ${this.marca}
+    Modelo: ${this.modelo}
+    Placa:  ${this.placa}
+    Color:  ${this.color}
+    kilometraje: ${this.kilometraje}
+        `
     }
 }
 
-const carroUno = new Vehiculo("2019", "Toyota", "KLX209", "Blanco", 0);
+const CarroUno = new Vehiculo("2019", "Toyota", "KLX209", "Blanco", 0);
 
 do{
     console.log("\n--- MENU DE COCHE ---");
@@ -49,20 +51,18 @@ do{
     console.log("5) Salor del menú\n");
     
     opcion = prompt("Selecciona una de las opciones: ");
-    
-    
 
     if(opcion == 1){
-        console.log(carroUno.informacionVehiculo());
+        console.log(CarroUno.informacionVehiculo());
 
     }else if(opcion == 2){
-        console.log(carroUno.encenderApagar());
+        console.log(CarroUno.encenderApagar());
 
     }else if(opcion == 3){
-        console.log(carroUno.avanzar());
+        console.log(CarroUno.avanzar());
 
     }else if(opcion == 4){
-        console.log(carroUno.parar());
+        console.log(CarroUno.parar());
     }else if(opcion == 5){
         stop = 0;
     }else{
